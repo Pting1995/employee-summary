a6b5bcf37491391e007d24803f317834e8e9ab60
Peter Ting <pting1995@gmail.com>
2021-01-23 01:43:47 -0800

    added employee sumarry to a empty array

--- app.js
+++ app.js
@@ -4,87 +4,107 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require('util');
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+teamArr = [];
 
 // manager
-inquirer.prompt([
-    {
-        type: "input",
-        message: "What is the manager's name?",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "What is the manager's ID?",
-        name: "id"
-    },
-    {
-        type: "input",
-        message: "What is the manager's email?",
-        name: "email"
-    },
-    {
-        type: "input",
-        message: "What is the manager's office number?",
-        name: "officeNumber"
-    }
-])
-// engineer
-inquirer.prompt([
-    {
-        type: "input",
-        message: "What is the engineer's name?",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "What is the engineer's ID?",
-        name: "id"
-    },
-    {
-        type: "input",
-        message: "What is the engineer's email?",
-        name: "email"
-    },
-    {
-        type: "input",
-        message: "What is the engineer's GitHub?",
-        name: "github"
-    }
-])
-// intern
-inquirer.prompt([
-    {
-        type: "input",
-        message: "What is the intern's name?",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "What is the intern's ID?",
-        name: "id"
-    },
-    {
-        type: "input",
-        message: "What is the intern's email?",
-        name: "email"
-    },
-    {
-        type: "input",
-        message: "What is the intern's college?",
-        name: "school"
-    }
-])
+const promptManager = () =>
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the manager's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is the manager's ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is the manager's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is the manager's office number?",
+            name: "officeNumber"
+        }
+    ]);
+
+promptManager()
+    .then(data => teamArr.push(new Manager(data.name, data.id, data.email, data.officeNumber)))
+    .then(() => askRole())
+
+const promptEng = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the engineer's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is the engineer's ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is the engineer's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is the engineer's GitHub?",
+            name: "github"
+        }
+    ])
+    .then(data => teamArr.push(new Engineer(data.name, data.id, data.email, data.github)))
+    .then(() => askRole())
+}
+
+
+const promptIntern = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the intern's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is the intern's ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is the intern's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is the intern's college?",
+            name: "school"
+        }
+    ])
+    .then(data => teamArr.push(new Intern(data.name, data.id, data.email, data.school)))
+    .then(() => askRole())
+}
 
 // ask what role next
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+
+// promptManager().then(askRole())
+// push object to end of teamArr
+
+// function render()
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
